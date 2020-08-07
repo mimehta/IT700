7500f10bfa05f6a20da30d07c94e841770aea519
Minesh Hasmukhlal Mehta <51768051+mineshmehta-dbconnect@users.noreply.github.com>
2020-08-07 10:28:29 -0400

    wip

--- R/uichartTab.R
+++ R/uichartTab.R
@@ -7,33 +7,37 @@ chartTab <- tabItem(
     box(includeHTML("www/chartbrief.html"), width = 12, background = "light-blue"),
   ),
   fluidRow(
-    box(title = "Distribution of charges", width = 4,collapsible = TRUE,
-        column(
+    box(title = "Distribution of charges", width = 12,collapsible = TRUE,
+        fluidRow(
+        column(width = 4,
           checkboxGroupInput("smoBox","",inline = TRUE,selected = c(2,1),
                          c("Smoker" = 2,"Non-smoker" = 1)
                          ) ,
-        plotOutput("smoDistPlot"),
-        p("The charges of most of the non-smoker patients are less than 15K and peak is around 4K."),
-        p("The charges of most of the smoker patients are more than 15K with two peaks at 20K and 40K."),
-        width = 12)
-    ),
-    box(width = 8, title = "Comparision of charges",collapsible = TRUE,
-        column( width = 4,
+          plotOutput("smoDistPlot")
+        ),
+        column( width = 3,
           plotOutput("smokerComp") 
         ), 
-        column( width = 8, 
+        column( width = 5, 
           plotOutput("smokercharts")
         ),
-        fluidRow(
-          column(width = 5,
-            p("The average charges:"),
-            p("Smoker ", strong(smokerMean, "$ ","Non-Smoker ", strong(nonsmokerMean, "$"))),
-          ),
-          column(width = 7,
-            p("The Smoker Male pays ", strong(diffMale, "$") , " higher than non-smoker."),
-            p("The Smoker Female pays ", strong(diffFemale, "$") , " higher than non-smoker."),
-          )
         ),
+#        fluidRow(
+          column(width = 4,
+                 p("The charges of most of the non-smoker patients are less than 15K and peak is around 4K."),
+                 p("The charges of most of the smoker patients are more than 15K with two peaks at 20K and 40K."),
+          ),
+          column(width = 3,
+                 p("The average charges:"),
+                 p("Smoker ", strong(smokerMean, "$ ","Non-Smoker ", strong(nonsmokerMean, "$"))),
+          ),
+          column(width = 5,
+                 p("The Smoker Male pays ", strong(diffMale, "$") , " higher than non-smoker."),
+                 p("The Smoker Female pays ", strong(diffFemale, "$") , " higher than non-smoker."),
+          )
+ #       )
+    ),
+    box(width = 12, title = "Comparision of charges",collapsible = TRUE,
         fluidRow(
           box(width = 12,
             p("dd")
